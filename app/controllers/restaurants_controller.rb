3f3b658484eeea6fc17c1c044d4e1b6266a86ffb
Benjamin McConnell <crosslegged@gmail.com>
2022-09-10 11:01:10 +0200

    gemfile & routes modified

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,4 +6,30 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(strong_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(strong_params)
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def strong_params
+    params.require(:restaurant).permit(:name, :address, :rating)
+  end
 end
