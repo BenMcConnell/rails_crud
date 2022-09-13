b1c247fa978f355efe374ac8a7ae9357dcc29eb1
Benjamin McConnell <crosslegged@gmail.com>
2022-09-13 14:23:35 +0200

    code refactored

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,12 @@
 class RestaurantsController < ApplicationController
+
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
   def index
     @restaurants = Restaurant.all
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
@@ -18,22 +20,23 @@ class RestaurantsController < ApplicationController
   end
 
   def edit
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def update
-    @restaurant = Restaurant.find(params[:id])
     @restaurant.update(strong_params)
     redirect_to restaurant_path(@restaurant)
   end
 
   def destroy
-    @restaurant = Restaurant.find(params[:id])
     @restaurant.destroy
-    redirect_to restaurants_path, status: :see_other
+    redirect_to restaurants_path, status: 303
   end
 
   private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 
   def strong_params
     params.require(:restaurant).permit(:name, :address, :rating)
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'restaurants', to: 'restaurants#index'
-  get 'restaurants/new', to: 'restaurants#new'
-  post 'restaurants', to: 'restaurants#create'
-  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
-  get 'restaurants/:id/edit', to: 'restaurants#edit'
-  patch 'restaurants/:id', to: 'restaurants#update'
-  delete 'restaurants/:id', to: 'restaurants#destroy'
+    # Read all
+  get "restaurants", to: "restaurants#index"
+  # Create
+  get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+  post "restaurants", to: "restaurants#create"
+  # Read one
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  # Update
+  get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+  patch "restaurants/:id", to: "restaurants#update"
+  # Delete
+  delete "restaurants/:id", to: "restaurants#destroy"
+
+  # resources :restaurants
 end
